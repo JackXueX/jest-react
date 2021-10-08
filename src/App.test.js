ee6e998b19b7de72bb6fef125c8f24b2cf22181e
JackXueX <15620606786@163.com>
2021-10-08 15:48:27 +0800

    💥 feat(模块): 添加了个enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import App from '../src/App';
+import { shallow } from 'enzyme'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('component APP test', () => {
+  it('测试hello world', () => {
+    const wrapper = shallow(<App />);
+    // console.log(wrapper.find('.app-container').length);
+    // expect(wrapper.find('.app-container').length).toBe(1);
+    // console.log(wrapper.find('.app-container').prop('title'));
+    // expect(wrapper.find('.app-container').prop('title')).toBe('dell lee');
+
+    // expect(wrapper.find('[data-test="container]').prop('title')).toBe('dell lee');
+
+    // console.log(wrapper.debug());
+    // 测试和代码逻辑解藕
+    // expect(wrapper.find('[data-test="container"]').prop('title')).toBe('dell lee');
+
+    // const container = wrapper.find('[data-test="container"]');
+
+    // expect(container).toExist();
+    // expect(container).toHaveProp('title', 'dell lee');
+
+    // 对于敏感内容的修改
+    expect(wrapper).toMatchSnapshot();
+
+  })
+})
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,10 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+// src/setupTests.ts
+
+// 导入 jest-enzyme 手动建立 enzyme 与 jest 的连接
+import 'jest-enzyme'
+
+// 启动 enzyme 并添加对应 react 版本的适配器
+import { configure } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+configure({ adapter: new Adapter() });
