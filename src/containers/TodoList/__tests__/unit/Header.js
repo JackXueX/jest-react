31e749154409611364d3cb1d393c838e4f5f9658
JackXueX <15620606786@163.com>
2021-10-08 18:37:54 +0800

    💥 feat(模块): 添加了通用代码抽取

--- src/containers/TodoList/__tests__/unit/Header.js
+++ src/containers/TodoList/__tests__/unit/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../../components/Header';
-import { shallow } from 'enzyme'
+import { shallow } from 'enzyme';
+import { findTestWrapper } from '../../../../utils/testUtils'
 
 describe('component APP test', () => {
   it('Header 渲染样式正常', () => {
@@ -10,19 +11,19 @@ describe('component APP test', () => {
 
   it('Header 组件包含一个 input 框', () => {
     const wrapper = shallow(<Header />);
-    const inputEle = wrapper.find("[data-test='input']");
+    const inputEle = findTestWrapper(wrapper, 'input');
     expect(inputEle.length).toBe(1);
   });
 
   it('Header 组件 input 框内容， 初始化应该为空', () => {
     const wrapper = shallow(<Header />);
-    const inputEle = wrapper.find("[data-test='input']");
+    const inputEle = findTestWrapper(wrapper, 'input');
     expect(inputEle.prop('value')).toEqual('');
   });
 
   it('Header 组件 input 框内容， 当用户输入时，会跟随变化', () => {
     const wrapper = shallow(<Header />);
-    const inputEle = wrapper.find("[data-test='input']");
+    const inputEle = findTestWrapper(wrapper, 'input');
 
     // 单元测试
     const userInput = '今天要学习 Jest';
@@ -41,7 +42,7 @@ describe('component APP test', () => {
   it('Header 组件 input 框输入回车时，如果 input 无内容，无操作', () => {
     const fn = jest.fn();
     const wrapper = shallow(<Header addUndoItem = {fn} />);
-    const inputEle = wrapper.find("[data-test='input']");
+    const inputEle = findTestWrapper(wrapper, 'input');
     wrapper.setState({
       value: ''
     })
@@ -54,7 +55,7 @@ describe('component APP test', () => {
   it('Header 组件 input 框输入回车时，如果 input 有内容，函数应该被调用', () => {
     const fn = jest.fn();
     const wrapper = shallow(<Header addUndoItem = {fn} />);
-    const inputEle = wrapper.find("[data-test='input']");
+    const inputEle = findTestWrapper(wrapper, 'input');
     wrapper.setState({
       value: '学习 React'
     })
@@ -68,14 +69,14 @@ describe('component APP test', () => {
   it('Header 组件 input 框输入回车时，如果 input 有内容，最后应该清除掉', () => {
     const fn = jest.fn();
     const wrapper = shallow(<Header addUndoItem = {fn} />);
-    const inputEle = wrapper.find("[data-test='input']");
+    const inputEle = findTestWrapper(wrapper, 'input');
     wrapper.setState({
       value: '学习 React'
     })
     inputEle.simulate('keyUp', {
       keyCode: 13
     });
-    const newInputEle = wrapper.find("[data-test='input']");
+    const newInputEle = findTestWrapper(wrapper, 'input');;
     expect(newInputEle.prop('value')).toBe('');
   });
 
